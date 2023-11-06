9323af663a2f7af6f0cfbcd7a961e5e4426d6f71
Rajmalpankaj786 <rajmalpankaj786@gmail.com>
2023-11-06 12:24:24 +0530

    add some images

--- Backend/ReadyToRide/src/main/java/com/ReadyToRide/Model/Admin.java
+++ Backend/ReadyToRide/src/main/java/com/ReadyToRide/Model/Admin.java
@@ -5,6 +5,6 @@ package com.ReadyToRide.Model;
 
 public class Admin {
 	public final Integer id=1;
-	public final String username="a@gmail.com";
+	public final String username="admin@gmail.com";
 	public final String password="123456";
 }
